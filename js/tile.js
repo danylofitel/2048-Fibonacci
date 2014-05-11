bc995c6da21a1dc7704765c68a865ce92be93f4e
Danylo Fitel <danylo.fitel@outlook.com>
2014-05-11 14:47:23 +0300

    Added Github Pages content.

---
+++ js/tile.js
@@ -0,0 +1,29 @@
+function Tile(position, number, value) {
+  this.x                = position.x;
+  this.y                = position.y;
+  this.number           = number || 1;
+  this.value            = value || 1;
+
+  this.previousPosition = null;
+  this.mergedFrom       = null; // Tracks tiles that merged together
+}
+
+Tile.prototype.savePosition = function () {
+  this.previousPosition = { x: this.x, y: this.y };
+};
+
+Tile.prototype.updatePosition = function (position) {
+  this.x = position.x;
+  this.y = position.y;
+};
+
+Tile.prototype.serialize = function () {
+  return {
+    position: {
+      x: this.x,
+      y: this.y
+    },
+	number: this.number,
+    value: this.value
+  };
+};
